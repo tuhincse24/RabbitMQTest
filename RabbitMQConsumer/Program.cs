e7ed754912eabf0dcfc9bd0925d5ba99167a36f1
tuhin <tuhin@test.com>
2019-11-12 22:53:30 +0600

    RabbitMQConsumer added.

--- RabbitMQConsumer/Program.cs
+++ RabbitMQConsumer/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 
 namespace RabbitMQConsumer
 {
     class Program
     {
+        private const string UName = "guest";
+        private const string Pwd = "guest";
+        private const string HName = "localhost";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HName,
+                UserName = UName,
+                Password = Pwd,
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            // accept only one unack-ed message at a time
+            // uint prefetchSize, ushort prefetchCount, bool global
+            channel.BasicQos(0, 1, false);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("topic.dhaka.queue", false, messageReceiver);
+            Console.ReadLine();
         }
     }
 }
+
