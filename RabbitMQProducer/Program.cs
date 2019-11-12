e0f177d4f8eb45c9816e577d111b7fba05a01244
tuhin <tuhin@test.com>
2019-11-12 22:18:59 +0600

    Add project files.

---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
